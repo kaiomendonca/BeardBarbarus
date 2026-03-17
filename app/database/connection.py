8a4002771d7b78e44ebe14b30923b824440cab71
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-03-16 21:25:54 -0300

    chore: add Base and SessionLocal imports to create tables

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -7,6 +8,8 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
 
 def test_connection():
     with engine.connect() as connection:
