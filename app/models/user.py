dbda97f3264bc1f7caf4a1fc3b3df97607caa1c4
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-03-16 21:27:05 -0300

    feat(models): create User table with UserRole enum and relationships

---
+++ app/models/user.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy.orm import relationship
+from database.connection import Base
+import Enum
+
+class UserRole(enum.Enum):
+    ADMIN = "admin"
+    PROFESSIONAL = "professional"
+    CLIENT = "client"
+    
+    
+class USER(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(UserRole), default=UserRole.CLIENT, nullable=False)
+    
+    appointments_as_client = relationship(
+        "Appointment",
+        back_populates= "client"
+        foreign_keys = "Appointment.user_id"
+    )
+    
+    appointments_as_professional = relationship(
+        "Appointment",
+        back_populates = "professional"
+        foreign_keys = "Appointment.professional_id"
+    )
+    
+
